2e3232e5fe303b5e1e21b8e4ee4ad851a4b26883
Raghavendra Nyshadham <rn@raghnysh.com>
2024-09-19 18:27:26 +0530

    Add settings for printing
    * configuration.nix: Add settings to facilitate printing.

--- configuration.nix
+++ configuration.nix
@@ -67,6 +67,9 @@
   ## =================================================================
 
   services.printing.enable = true;
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+  services.avahi.openFirewall = true;
 
   ## =================================================================
   ## Sound
